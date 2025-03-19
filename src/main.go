9637980f8ee373836f36192b446d4942993674cc
Arijit Das [Itobuz] <arijit@itobuz.com>
2025-03-19 13:38:02 +0530

    feat: init

--- src/main.go
+++ src/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/arijit-itobuz/go-rest/src/config"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 
 	log.Printf("[main]: AppConfig: %+v\n", config.AppConfig)
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Addr:    ":" + config.AppConfig.PORT,
+		Handler: router,
+	}
+
+	log.Printf("[main]: Server starting on http://localhost:%v\n", config.AppConfig.PORT)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("[main]: Error %v", err)
+	}
 
 }
